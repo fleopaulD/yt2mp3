aaa24784aeb90d13d832f57589e92861aadaff5f
fleopaulD <flo_thp@netc.pl>
2023-09-27 10:36:57 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/bash -e
+#!/usr/bin/bash
 
 cd /tmp || exit 1
 
-wget https://raw.githubusercontent.com/fleopaulD/yt2mp3/main/yt2mp3.sh
+mkdir yt2mp3 && cd yt2mp3 || exit 1
+wget https://github.com/fleopaulD/yt2mp3/archive/refs/heads/main.zip -O yt2mp3.zip
+unzip yt2mp3.zip 
+cd yt2mp3-main || exit 1
+ 
+sudo cp yt2mp3.sh /usr/local/bin/yt2mp3 && sudo chmod +x /usr/local/bin/yt2mp3
+cp "Youtube en MP3.desktop" "$HOME/Bureau/"
 
-sudo cp yt2mp3.sh /usr/local/bin/yt2mp3
+cd /tmp || exit 1
+rm -rf yt2mp3*
